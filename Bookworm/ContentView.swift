a033c3210f952546b4fbd5909655dcb85aa1a79b
Tim-engineer <matlakt@outlook.com>
2024-06-26 11:03:25 +0200

    Working SwiftData

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -6,16 +6,44 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddView = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(books) { book in
+                    NavigationLink(value: book) {
+                        HStack {
+                            EmojiRatingView(rating: book.rating)
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title)
+                                    .font(.headline)
+                                Text(book.author)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Add book", systemImage: "plus") {
+                        showingAddView.toggle()
+                    }
+                }
+            }
+            .sheet(isPresented: $showingAddView) {
+                AddBookView()
+            }
         }
-        .padding()
     }
 }
 
